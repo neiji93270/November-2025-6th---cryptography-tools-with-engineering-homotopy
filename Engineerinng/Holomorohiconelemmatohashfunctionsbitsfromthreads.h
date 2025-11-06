4624dbe184196e17bec06de43d202eedbf6c05f6
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-06 13:12:03 +0100

    Create Holomorohiconelemmatohashfunctionsbitsfromthreads.h

---
+++ Engineerinng/Holomorohiconelemmatohashfunctionsbitsfromthreads.h
@@ -0,0 +1,5 @@
+//threads in this program could stip at one step if the low level system accept bits from cpu.
+//then the usb key would have a long range hash lets say 10^17 or 10^15 digits. but the main problem is still
+//the short number of rainbow attack equations parametere.
+//as an engineerin result, we would better have oarc model expansion parametere.
+//the latent space could be then fullfilled with variational methods which allow matlab to be used.
