66ebfda31f06fd337bffb034c796dae0c1a55b7f
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-06 12:37:47 +0100

    Create Baserainbowheaders.h

---
+++ Engineering/Baserainbowheaders.h
@@ -0,0 +1,5 @@
+//the loop in some topologiczl spaces can be enforced by
+//a lower power cyber supply chain than high energy sustems. 
+//then, we are able to separate rainbow attack algorithm intontwo threads
+//that are suited for stuxnet. although the private keyd are wrong,
+/we are free to add pro p groups too. then somenof our engineering stuffs are not isomorphic.
