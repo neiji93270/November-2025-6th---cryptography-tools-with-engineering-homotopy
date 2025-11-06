ec9e6a4dd50ac5c902251f4ed588b0f6cd37aa9a
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-06 18:01:59 +0100

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,7 @@
+//1000 is a valeu strange ! we must use ln(exp(1000)). in modulos,
+//it is good to add several excursions. so lets take 1000 again.
+//we finally have the threads number which is close to 1 millions. then the usb device
+//like in another project, must communicate with a super computer.
+//so here hierarchical lear ing doesnt need more than 1000 mean filed games if
+//the pro p groups are isomorph to sole of log(1000) equations.
+//we then optimized rainbow attack
